b54d9582f389ba31a27f609874611c472b901bab
ellreka <ellreka5364@gmail.com>
2022-03-11 12:53:47 +0900

    feat: update

--- index.js
+++ index.js
@@ -10,61 +10,67 @@ const process = require("process");
   if (!["start", "end"].includes(type)) throw new Error("無効なコマンドです");
 
   const browser = await puppeteer.launch({
-    headless: true,
+    headless: false,
     devtools: false,
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
   });
+  try {
+    // 位置情報の取得を許可
+    const context = browser.defaultBrowserContext();
+    await context.overridePermissions(undefined, ["geolocation"]);
 
-  const context = browser.defaultBrowserContext();
-  await context.overridePermissions(undefined, ["geolocation"]);
+    const page = (await browser.pages())[0];
+    page.setViewport({ width: 1920, height: 1080 });
 
-  const page = (await browser.pages())[0];
+    // login
+    await page.goto("https://teamspirit.cloudforce.com/");
+    await page.type('input[name="username"]', config.id);
+    await page.type('input[name="pw"]', config.password);
+    await page.click('input[name="Login"]');
 
-  await page.goto("https://teamspirit.cloudforce.com/");
+    await page.waitForTimeout(10000);
 
-  // login
-  await page.type('input[name="username"]', config.id);
-  await page.type('input[name="pw"]', config.password);
-  await page.click('input[name="Login"]');
+    // iframeを取得
+    const frame = await page
+      .frames()
+      .find((frame) => frame.name().includes("vfFrameId"));
 
-  await page.waitForTimeout(10000);
+    switch (type) {
+      // 出勤ボタンを押す
+      case "start":
+        await frame
+          .waitForSelector(`${startBtnSelector}:not(:disabled)`, {
+            timeout: 10000,
+            visible: true,
+          })
+          .catch((err) => {
+            console.error("出勤出来ませんでした");
+            throw err;
+          });
 
-  const frame = await page
-    .frames()
-    .find((frame) => frame.name().includes("vfFrameId"));
-
-  switch (type) {
-    case "start":
-      await frame
-        .waitForSelector(startBtnSelector, {
-          timeout: 10000,
-          visible: true,
-        })
-        .catch((err) => {
-          console.error("出勤出来ませんでした");
-          throw err;
-        });
-      await frame.click(startBtnSelector);
-      await frame.waitForSelector(`${startBtnSelector}[disabled]`);
-      break;
-    case "end":
-      await frame
-        .waitForSelector(endBtnSelector, {
-          timeout: 10000,
-          visible: true,
-        })
-        .catch((err) => {
-          console.error("退勤出来ませんでした");
-          throw err;
-        });
-      await frame.click(endBtnSelector);
-      await frame.waitForSelector(`${endBtnSelector}[disabled]`);
-      break;
-    default:
-      console.error("error");
+        await frame.click(startBtnSelector);
+        await frame.waitForSelector(`${startBtnSelector}[disabled]`);
+        break;
+      case "end":
+        // 退勤ボタンを押す
+        await frame
+          .waitForSelector(`${endBtnSelector}:not(:disabled)`, {
+            timeout: 10000,
+            visible: true,
+          })
+          .catch((err) => {
+            console.error("退勤出来ませんでした");
+            throw err;
+          });
+        await frame.click(endBtnSelector);
+        await frame.waitForSelector(`${endBtnSelector}[disabled]`);
+        break;
+      default:
+        console.error("error");
+    }
+  } catch (err) {
+    console.error(err);
+  } finally {
+    await browser.close();
   }
-
-  await page.waitForTimeout(3000);
-
-  await browser.close();
 })();
