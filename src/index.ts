c11eb3c6c0b1a9dadaaf8079a6ea202c1a192aab
ellreka <ellreka5364@gmail.com>
2022-10-17 18:18:20 +0900

    feat: add typescript

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,33 @@
-const puppeteer = require("puppeteer");
-const config = require("./config.json");
-const process = require("process");
+import puppeteer from "puppeteer";
+import config from "../config.json";
+import process from "process";
 
 (async () => {
   const startBtnSelector = "#btnStInput";
   const endBtnSelector = "#btnEtInput";
 
   const type = process.argv[2];
-  if (!["start", "end"].includes(type)) throw new Error("無効なコマンドです");
+  if (!["start", "end", "input"].includes(type))
+    throw new Error("無効なコマンドです");
 
   const browser = await puppeteer.launch({
     headless: false,
-    devtools: false,
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    args: [
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--disable-notifications",
+    ],
   });
   try {
     // 位置情報の取得を許可
     const context = browser.defaultBrowserContext();
-    await context.overridePermissions(undefined, ["geolocation"]);
+    await context.overridePermissions(config.url, ["geolocation"]);
 
     const page = (await browser.pages())[0];
     page.setViewport({ width: 1920, height: 1080 });
 
     // login
-    await page.goto("https://teamspirit.cloudforce.com/");
+    await page.goto(config.url);
     await page.type('input[name="username"]', config.id);
     await page.type('input[name="pw"]', config.password);
     await page.click('input[name="Login"]');
@@ -34,7 +38,7 @@ const process = require("process");
     const frame = await page
       .frames()
       .find((frame) => frame.name().includes("vfFrameId"));
-
+    if (frame == null) throw new Error("iframeが取得できませんでした");
     switch (type) {
       // 出勤ボタンを押す
       case "start":
@@ -65,6 +69,35 @@ const process = require("process");
         await frame.click(endBtnSelector);
         await frame.waitForSelector(`${endBtnSelector}[disabled]`);
         break;
+      // case "input":
+      //   await page.goto(
+      //     `${config.url}/lightning/n/teamspirit__AtkWorkTimeTab`
+      //   );
+      //   await page.waitForTimeout(10000);
+      //   // iframeを取得
+      //   const iframe = await page
+      //     .frames()
+      //     .find((frame) => frame.name().includes("vfFrameId"));
+      //   if (iframe == null) throw new Error("iframeが取得できませんでした");
+      //   await iframe.click("#dailyWorkCell2022-07-01");
+      //   await page.waitForTimeout(10000);
+      //   const table = await iframe.$("#empWorkTableBody");
+      //   console.log(table);
+      //   const title = (
+      //     await table?.$x(
+      //       `//div[@class = "name" and text() = "`
+      //     )
+      //   )?.[0];
+      //   console.log(title);
+      //   if (title == null) return;
+      //   const cell = (await title.getProperty("parentNode")).asElement();
+      //   console.log(cell);
+      //   if (cell == null) return;
+      //   const input = await cell.$("input#empInputTime0");
+      //   console.log(input);
+      //   input?.type("7:10");
+      //   await page.waitForTimeout(10000);
+      //   break;
       default:
         console.error("error");
     }
